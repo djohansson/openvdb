979aafb12ed2afd7d0b9469052ed1d0e7a48a965
Daniel Johansson <danjo0081979@gmail.com>
2018-03-20 13:28:36 +0100

    tbb mutex replaced with std::mutex

--- openvdb/python/pyutil.h
+++ openvdb/python/pyutil.h
@@ -155,12 +155,12 @@ struct StringEnum
     /// Return the (key, value) map as a Python dict.
     static boost::python::dict items()
     {
-        static tbb::mutex sMutex;
+        static std::mutex sMutex;
         static boost::python::dict itemDict;
         if (!itemDict) {
             // The first time this function is called, populate
             // the static dict with (key, value) pairs.
-            tbb::mutex::scoped_lock lock(sMutex);
+            std::lock_guard<std::mutex> lock(sMutex);
             if (!itemDict) {
                 for (int i = 0; ; ++i) {
                     const CStringPair item = Descr::item(i);
